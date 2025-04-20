ea870de8b4335594d231b9ce11f9d198e77b88e6
Waritnun Sakulklang <93075364+waritnun@users.noreply.github.com>
2025-04-20 14:29:10 +0700

    Recursive

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(n + "! : " + fact(n));
+    }
+    public static int fact(int n ){
+        if (n == 0){
+            return 1;
+        }
+        else{
+            return n * fact(n-1);
+        }
+    }
+
+}
---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println("Fibonacci number of " + n + " : " + fibo(n));
+    }
+    public static int fibo(int n ){
+        if (n >=3){
+            return fibo(n-1) + fibo(n-2);
+        }
+        else{
+            return 1;
+        }
+    }
+
+}
